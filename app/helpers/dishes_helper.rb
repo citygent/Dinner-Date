caa5eea1620a5940b32cafe95b3f0c9d96d771d1
James White <cyberyt@gmail.com>
2015-09-19 15:31:30 +0100

    added restaurant index and show

--- app/helpers/dishes_helper.rb
+++ app/helpers/dishes_helper.rb
@@ -3,7 +3,7 @@ module DishesHelper
     dish.orders.map(&:cost).inject(:+) / dish.orders.size
   end
 
-  def average_rating(dish)
+  def average_rating(dish) #or restaurant
     dish.orders.map(&:rating).inject(:+) / dish.orders.size
   end
 end
